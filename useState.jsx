c04155a362464a026fe33c874a2d0ff1525cfe71
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-06-28 09:14:57 +0530

    Update useState.jsx

--- useState.jsx
+++ useState.jsx
@@ -1,1 +1,22 @@
+import { useState } from 'react'
 
+function Counter() {
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      
+      <button onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+      
+      <button onClick={() => setCount(0)}>
+        Reset
+      </button>
+    </div>
+  )
+}
+
+export default Counter
