276c344f04453102d1aa183e2597a20c59e8a099
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-06-29 09:15:03 +0530

    Update useEffect.jsx

--- useEffect.jsx
+++ useEffect.jsx
@@ -1,1 +1,20 @@
+import { useState, useEffect } from "react";
 
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
