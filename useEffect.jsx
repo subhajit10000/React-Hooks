b632c38554f8f6635264e1d6cef86df9aec265f8
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-06-29 09:17:49 +0530

    Update useEffect.jsx

--- useEffect.jsx
+++ useEffect.jsx
@@ -1,3 +1,5 @@
+                //example 1 //
+
 import { useState, useEffect } from "react";
 
 function Counter() {
@@ -18,3 +20,33 @@ function Counter() {
 }
 
 export default Counter;
+
+
+
+
+
+              //example 2 //
+
+import { useState, useEffect } from "react";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+
+      {users.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </div>
+  );
+}
+
+export default Users;
